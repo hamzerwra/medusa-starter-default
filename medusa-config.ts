b4d2b82a28a412cec88212ec6d6728a7363c80cf
Hamza <rajhamza45@gmail.com>
2025-04-10 12:43:29 +0500

    fix: properly import defineConfig and disable redis workflow

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,3 +1,8 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+
 export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
